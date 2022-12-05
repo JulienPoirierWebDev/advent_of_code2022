9f44924345d4f4d79e12765ecb8014e5d5900059
Julien POIRIER <connexion@julienpoirier-webdev.com>
2022-12-05 08:21:24 +0100

    day4 part 1 2

--- scripts/day4.js
+++ scripts/day4.js
@@ -1,10 +1,6 @@
-function splittingAssignments(ranges) {
-    console.log(ranges.split(" "))
-    return ranges.split(" ")
-}
 
 function fullyContain(ranges) {
-    let splitting = ranges.split(" ")
+    let splitting = ranges.split(",")
     let range1 = splitting[0];
     let range2 = splitting[1];
 
@@ -15,7 +11,6 @@ function fullyContain(ranges) {
     let range2Splitting = range2.split("-");
     let min2 = Number(range2Splitting[0]);
     let max2 = Number(range2Splitting[1]);
-    console.log(range1Splitting, range2Splitting)
 
     if(min1 >= min2 && max1 <= max2) {
         return true;
@@ -27,4 +22,36 @@ function fullyContain(ranges) {
 
 }
 
-module.exports = {splittingAssignments, fullyContain}
+function partiallyContain(ranges) {
+    let splitting = ranges.split(",")
+    let range1 = splitting[0];
+    let range2 = splitting[1];
+
+    let range1Splitting = range1.split("-");
+    let min1 = Number(range1Splitting[0]);
+    let max1 = Number(range1Splitting[1]);
+
+    let range2Splitting = range2.split("-");
+    let min2 = Number(range2Splitting[0]);
+    let max2 = Number(range2Splitting[1]);
+
+    if(min1 >= min2 && max1 <= max2) {
+        return true;
+    } else if(min2 >= min1 && max2 <= max1) {
+        return true;
+    } else if(min1 <= min2 && max1 <= max2 && max1 >= min2) {
+        return true;
+    } else if(min2 <= min1 && max2 <= max1 && max2 >= min1) {
+        return true;
+    }
+
+
+
+    else {
+        return false;
+    }
+
+}
+
+module.exports = {fullyContain, partiallyContain}
+
--- test/day4.test.js
+++ test/day4.test.js
@@ -1,17 +1,48 @@
-const { fullyContain } = require("../scripts/day4");
+const { fullyContain, partiallyContain } = require("../scripts/day4");
 
 describe("Assignement fully contain the other", () => {
 
     test("should return true when fully contain", () => {
-        expect(fullyContain("50-50 50-87")).toEqual(true);
+        expect(fullyContain("50-50,50-87")).toEqual(true);
     })
 
     test("should same", () => {
-        expect(fullyContain("2-92 2-86")).toEqual(true)
+        expect(fullyContain("2-92,2-86")).toEqual(true)
     })
 
     test("should return false when not contain at all", () => {
-        expect(fullyContain("8-15 18-50")).toEqual(false)
+        expect(fullyContain("8-15,18-50")).toEqual(false)
+    })
+
+})
+
+describe("Assignement partially contain the other", () => {
+    test("should return true when fully contain", () => {
+        expect(partiallyContain("50-50,50-87")).toEqual(true);
+    })
+
+    test("should same", () => {
+        expect(partiallyContain("2-92,2-86")).toEqual(true)
+    })
+
+    test("should return false when not contain at all", () => {
+        expect(partiallyContain("8-15,18-50")).toEqual(false)
+    })
+
+    test("should return when partially contain", () => {
+        expect(partiallyContain("5-7,7-9")).toEqual(true)
+    })
+
+    test("should return when partially contain", () => {
+        expect(partiallyContain("6-6,4-6")).toEqual(true)
+    })
+
+    test("should return when partially contain", () => {
+        expect(partiallyContain("2-6,4-8")).toEqual(true)
+    })
+
+    test("should return when partially contain", () => {
+        expect(partiallyContain("4-8,2-6")).toEqual(true)
     })
 
 })
