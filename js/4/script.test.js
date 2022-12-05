e70bd54ba681aa68e774f603dd5e1968dc42c6fc
Julien POIRIER <connexion@julienpoirier-webdev.com>
2022-12-05 06:34:25 +0100

    install JEST and prepare first test

---
+++ js/4/script.test.js
@@ -0,0 +1,5 @@
+describe("Assignement fully contain the other", () => {
+    test("should return true when fully contain", () => {
+        expect(fullyContain("50-50 50-87").expect(true))
+    })
+})
